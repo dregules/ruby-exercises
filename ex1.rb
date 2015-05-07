66227edb3d05139b3bbd00523ba919ab5162e6b8
Diego Regules <dregules@users.noreply.github.com>
2015-05-07 13:22:56 -0400

    First commit

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "hello world"
+puts "my name is Diego"
+puts "I like Typing this"
+puts "this is really my first ruby program"
+puts "oh well, here it comes..."
+puts "I'd rather must you 'not'."
+puts
+puts 'I "said" do not touch this.'	
+#probably a comment on the code
