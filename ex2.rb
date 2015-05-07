3e866978e14be5f07189332c1d3c7e9606abe56c
Diego Regules <dregules@users.noreply.github.com>
2015-05-07 14:17:13 -0400

    Second commit- Exercise 2

---
+++ ex2.rb
@@ -0,0 +1,6 @@
+puts "I will now count the chickens"
+puts "Hens #{25 + 30 / 6}" #It looks like #symbol automatically 
+							#prompts to do a calculation
+puts "is it true that 3 + 2 < 5 - 7"
+puts 3 + 2 < 5 - 7
+puts "should be False above"
